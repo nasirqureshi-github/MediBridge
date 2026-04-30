af3a803239711c22edec69e85873880d4bad194a
Nasir qureshi <138995290+nasirqureshi-github@users.noreply.github.com>
2026-05-01 01:17:34 +0500

    initial commit

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>Hi</h1>
+        </div>
+  );
+}
